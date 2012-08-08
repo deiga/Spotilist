b60d1b1b6368b6b668995d9c20ba8d3640c30628
Timo Sand <timo.j.sand@gmail.com>
2012-08-08 13:49:45 +0300

    FIXED: Indentation

--- spotilist.rb
+++ spotilist.rb
@@ -2,28 +2,28 @@
 require 'hallon'
 
 class Spotilist < Sinatra::Base
-	configure :development do
-		register Sinatra::Reloader
-	end
+  configure :development do
+    register Sinatra::Reloader
+  end
 
-	Hallon.load_timeout = 60.0
+  Hallon.load_timeout = 60.0
 
-	get '/:uri' do
-		if Hallon::Session.instance?
-			session = Hallon::Session.instance
-		else
-			session = Hallon::Session.initialize IO.read('./spotify_appkey.key')
-		end
-		unless Hallon::Link.valid?(params[:uri])
-			"Given URI was not a valid spotify URI"
-		end
-		session.login!(ENV['SPOTIFY_USRNM'], ENV['SPOTIFY_PWD'])
-		uri = params[:uri]
-		playlist_link = Hallon::Link.new(uri)
-		
-		@playlist = Hallon::Playlist.new(playlist_link).load
-		@tracks = @playlist.tracks
-		
-		haml :index
-	end
+  get '/:uri' do
+    if Hallon::Session.instance?
+      session = Hallon::Session.instance
+    else
+      session = Hallon::Session.initialize IO.read('./spotify_appkey.key')
+    end
+    unless Hallon::Link.valid?(params[:uri])
+      "Given URI was not a valid spotify URI"
+    end
+    session.login!(ENV['SPOTIFY_USRNM'], ENV['SPOTIFY_PWD'])
+    uri = params[:uri]
+    playlist_link = Hallon::Link.new(uri)
+    
+    @playlist = Hallon::Playlist.new(playlist_link).load
+    @tracks = @playlist.tracks
+    
+    haml :index
+  end
 end
