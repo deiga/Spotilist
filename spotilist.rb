31b6cc99414114d8253e4d67c994b77284429dec
Timo Sand <timo.j.sand@gmail.com>
2012-08-08 13:23:11 +0300

    CHANGED: Getter to load spotify playlists

--- spotilist.rb
+++ spotilist.rb
@@ -1,9 +1,27 @@
 # spotilist.rb
+require 'hallon'
+
 class Spotilist < Sinatra::Base
-	get '/' do
-		"Hello World"
 	configure :development do
 		register Sinatra::Reloader
 	end
+
+	Hallon.load_timeout = 60.0
+
+	get '/:uri' do
+		if Hallon::Session.instance?
+			session = Hallon::Session.instance
+		else
+			session = Hallon::Session.initialize IO.read('./spotify_appkey.key')
+		end
+		unless Hallon::Link.valid?(params[:uri])
+			"Given URI was not a valid spotify URI"
+		end
+		session.login!(ENV['SPOTIFY_USRNM'], ENV['SPOTIFY_PWD'])
+		uri = params[:uri]
+		playlist_link = Hallon::Link.new(uri)
+		@playlist = Hallon::Playlist.new(playlist_link).load
+		@tracks = @playlist.tracks#.to_a.map(&:load)
+		haml :index
 	end
 end
