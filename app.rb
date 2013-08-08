d030e920de2b978864cf3b3173f173b49801b53a
Timo Sand <timo.j.sand@gmail.com>
2013-08-08 17:51:12 +0300

    Added better duration calculation

--- app.rb
+++ app.rb
@@ -46,6 +46,38 @@ class Spotilist < Sinatra::Base
     set :show_exceptions, false #true will ignore raise_errors and display backtrace in browser
   end
 
+  helpers do
+
+    def duration_in_minutes(time_in_seconds)
+      duration(time_in_seconds, 'm')
+    end
+
+    def duration_in_hours(time_in_seconds)
+      duration(time_in_seconds, 'h')
+    end
+
+    def duration(time_in_seconds, format)
+      mm, ss = time_in_seconds.to_i.divmod(60)
+      case format
+      when 'h'
+      hh, mm = mm.divmod(60)
+      mm = add_leading_zero(mm)
+      hh = add_leading_zero(hh)
+        "#{hh}:#{mm}"
+      when 'm'
+      ss = add_leading_zero(ss)
+      mm = add_leading_zero(mm)
+        "#{mm}:#{ss}"
+      else
+        time_in_seconds
+      end
+    end
+
+    def add_leading_zero(number)
+      number.to_s.length < 2 ? "0#{number}" : number
+    end
+  end
+
   at_exit do
     if Hallon::Session.instance?
       hallon = Hallon::Session.instance
